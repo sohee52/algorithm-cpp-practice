40639651c40b0430ce04f09d45e370dcdd9603bf
sohee52 <wneh2023@naver.com>
2025-10-31 20:41:47 +0900

    2751. 수 정렬하기 2

---
+++ class2/2751.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, temp;
+vector<int> v;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> temp;
+        v.push_back(temp);
+    }
+    sort(v.begin(), v.end());
+    for (int a : v) cout << a << "\n";
+}
