7edec37f429693c54e9a7637a9dc97967622f4dc
sohee52 <wneh2023@naver.com>
2025-12-03 00:28:30 +0900

    1541. 잃어버린 괄호

---
+++ class3/1541.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+
+void zero(string s) {
+    int i  = 1;
+    while (true) {
+        s[i]
+    }
+}
+
+void parse(string s) {
+    if (s[0] == '0') {
+        
+    } 
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> s;
+
+    parse(s);
+}
