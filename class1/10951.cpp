bf47fc8562e53c68c5f65a4778ee428779dab7ef
sohee52 <wneh2023@naver.com>
2026-03-02 23:22:43 +0900

    solve: A+B - 4

---
+++ class1/10951.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, b;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+    while(cin >> a >> b) {
+        cout << a + b << "\n";
+    }
+}
