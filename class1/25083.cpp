1ab86287c5c6f1c74e65fc89704af5ca137459b6
sohee52 <wneh2023@naver.com>
2026-03-03 01:01:47 +0900

    solve: 새싹

---
+++ class1/25083.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout << R"(         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |)";
+}
