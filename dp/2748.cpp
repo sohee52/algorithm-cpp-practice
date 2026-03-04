6dd8f8cf53904321879d2d1ba17cadd2d5db8d45
sohee52 <wneh2023@naver.com>
2026-03-04 16:49:47 +0900

    solve: 피보나치 수 2

---
+++ dp/2748.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long
+ll n;
+vector<ll> v(24);
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+    cin >> n;
+    v[0] = 0; v[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        v[i] = v[i-1] + v[i-2];
+    }
+    cout << v[n] << "\n";
+}
