4fa84a150eab1d3eea012c6d74e27c724b82aa48
sohee52 <wneh2023@naver.com>
2026-03-04 18:11:18 +0900

    refactor: 돌 게임 dp 풀이

--- dp/9655.cpp
+++ dp/9655.cpp
@@ -1,11 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 int n;
-
+ll dp[1004];
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-	cin >> n;
-    if (n % 2 == 1) cout << "SK" << "\n";
+	
+    cin >> n;
+
+    // if (n % 2 == 1) cout << "SK" << "\n";
+    // else cout << "CY" << "\n";
+
+    // dp 버전
+    dp[1] = 1;
+    dp[2] = 0;
+    dp[3] = 1;
+
+    for (int i = 4; i <= n; i++) {
+        dp[i] = (!dp[i-1]) || (!dp[i-3]);
+    }
+
+    if (dp[n]) cout << "SK" << "\n";
     else cout << "CY" << "\n";
 }
