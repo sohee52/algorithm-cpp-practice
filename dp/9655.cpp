350cdc952e38de587a5e3930cd19d53e8a41928e
sohee52 <wneh2023@naver.com>
2026-03-04 17:50:31 +0900

    solve: 돌 게임

---
+++ dp/9655.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cin >> n;
+    if (n % 2 == 1) cout << "SK" << "\n";
+    else cout << "CY" << "\n";
+}
